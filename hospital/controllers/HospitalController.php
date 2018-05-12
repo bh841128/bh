7b889e7fb5a13f086a648962a7d226d60e04cf2c
bh841128 <390214830@qq.com>
2018-05-12 12:44:27 +0800

    dfgdfg

--- hospital/controllers/HospitalController.php
+++ hospital/controllers/HospitalController.php
@@ -1,5 +1,4 @@
 <?php
-
 namespace app\controllers;
 
 use Yii;
@@ -9,9 +8,12 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\CUtil;
 use app\models\Login4Hospital;
+use app\models\Patient4Hospital;
 use yii\log\Logger;
 
 define("NOLOGIN", 1);
+define("NODATA", 2);
+define("NOACCESS", 3);
 
 
 class HospitalController extends Controller
@@ -61,21 +63,7 @@ class HospitalController extends Controller
   
 	
 
-     public function actionGetManager()
-    {
-        $username = CUtil::getRequestParam('request', 'username', '');
-        $skey = CUtil::getRequestParam('cookie', 'skey', '');
-        $ret=Login4Hospital::checkLogin($username,$skey);
-        CUtil::logFile("====".print_r($ret,true));
-        if($ret["ret"]!=0){
-            $ret["ret"]=NOLOGIN;
-            $ret["msg"]="not login";
-            return json_encode($ret);
-        }
-        $ret=Login4Hospital::getManager($username);
-        
-        return json_encode($ret);
-    }
+  
 
     public function actionModpwd()
     {
@@ -85,7 +73,7 @@ class HospitalController extends Controller
         
         //echo("===$username  $skey    ");
         $ret=Login4Hospital::modpassword($username,$oldpassword,$newpassword);
-        
+        CUtil::logFile("not login====".print_r($ret,true));
         return json_encode($ret);
     }
 
@@ -96,7 +84,7 @@ class HospitalController extends Controller
 		
 		//echo("===$username  $skey    ");
 		$ret=Login4Hospital::checkLogin($username,$skey);
-		
+		//CUtil::logFile("not login====".print_r($ret,true));
         return json_encode($ret);
     }
 	
@@ -107,12 +95,99 @@ class HospitalController extends Controller
 		$password = CUtil::getRequestParam('req', 'password', '');
 		$ret=Login4Hospital::loginIn($username,$password);
 		
-		
+		CUtil::logFile("not login====".print_r($ret,true));
 		
         return json_encode($ret);
     }
 	
 	
+    public function actionGetManager()
+    {
+        $username = CUtil::getRequestParam('request', 'username', '');
+        $skey = CUtil::getRequestParam('cookie', 'skey', '');
+        $ret=Login4Hospital::checkLogin($username,$skey);
+        CUtil::logFile("====".print_r($ret,true));
+        if($ret["ret"]!=0){
+            $ret["ret"]=NOLOGIN;
+            $ret["msg"]="not login";
+			CUtil::logFile("not login====".print_r($ret,true));
+            return json_encode($ret);
+        }
+        $ret=Login4Hospital::getManager($username);
+         if($ret["ret"]!=0){
+            $ret["ret"]=NODATA;
+            $ret["msg"]="no data";
+			CUtil::logFile("not login====".print_r($ret,true));
+            return json_encode($ret);
+        }
+        return json_encode($ret);
+    }
+
+	public function actionGetPatient()
+    {
+        $username = CUtil::getRequestParam('cookie', 'username', '');
+        $id = CUtil::getRequestParam('request', 'id', 0);
+        $skey = CUtil::getRequestParam('cookie', 'skey', '');
+        $ret=Login4Hospital::checkLogin($username,$skey);
+        CUtil::logFile("====".print_r($ret,true));
+        if($ret["ret"]!=0){
+            $ret["ret"]=NOLOGIN;
+            $ret["msg"]="not login";
+			CUtil::logFile("not login====".print_r($ret,true));
+            return json_encode($ret);
+        }
+		//登录
+		$ret=Login4Hospital::getManager($username);
+         if($ret["ret"]!=0){
+            $ret["ret"]=NOACCESS;
+            $ret["msg"]="no ACCESS";
+			CUtil::logFile("not login====".print_r($ret,true));
+            return json_encode($ret);
+        }
+		
+		//获取管理人员信息
+        $ret=Patient4Hospital::getPatientById($id,$ret["msg"]["hospital_id"]);
+        if($ret["ret"]!=0){
+            $ret["ret"]=NODATA;
+            $ret["msg"]="no data";
+			CUtil::logFile("not login====".print_r($ret,true));
+            return json_encode($ret);
+        }
+        return json_encode($ret);
+    }
 	
+	//status  0:正常 1:上传  2：删除
+	public function actionSetPatientsStatus()
+    {
+        $username = CUtil::getRequestParam('cookie', 'username', '');
+        $ids = CUtil::getRequestParam('request', 'ids', "");
+		$status = CUtil::getRequestParam('request', 'status', 0);
+        $skey = CUtil::getRequestParam('cookie', 'skey', '');
+		//登录
+        $ret=Login4Hospital::checkLogin($username,$skey);
+        if($ret["ret"]!=0){
+            $ret["ret"]=NOLOGIN;
+            $ret["msg"]="not login";
+			CUtil::logFile("not login====".print_r($ret,true));
+            return json_encode($ret);
+        }
+		//获取管理人员信息
+		$ret=Login4Hospital::getManager($username);
+         if($ret["ret"]!=0){
+            $ret["ret"]=NOACCESS;
+            $ret["msg"]="no ACCESS";
+			CUtil::logFile("not login====".print_r($ret,true));
+            return json_encode($ret);
+        }
+		
+		$idarray=explode(",",$ids);
+        $ret=Patient4Hospital::setPatientStatusByIds($idarray,$ret["msg"]["hospital_id"],$status,$ret["msg"]["id"]);
+        if($ret["ret"]!=0){
+            CUtil::logFile("setPatientStatusByIds err====".print_r($ret,true));
+            return json_encode($ret);
+        }
+        return json_encode($ret);
+    }
+
 
 }
--- hospital/models/Patient4Hospital.php
+++ hospital/models/Patient4Hospital.php
@@ -6,11 +6,93 @@ use Yii;
 use app\models\CError;
 use app\models\CUtil;
 
-class User4Hospital {
+class Patient4Hospital {
+
+	static public function getPatientById($id,$hospital_id)
+	 {
+		 $ret=array(
+		   "ret"=>0,
+		   
+		   "msg"=>""
+		);
+		
+		$sql = "select * from patientInfo where id=:id and hospital_id=:hospital_id ";
+		$args=array(':id'=>$id,':hospital_id'=>$hospital_id);
+		CUtil::logFile("=====$id  ");
+		
+		$connection = Yii::$app->db;
+		$command = $connection->createCommand($sql,$args);
+		$records = $command->queryAll();
+		if(count($records)!=1){
+			$ret["ret"]=1;
+						
+			return $ret;
+		}
+		$ret["msg"]=$records[0];
+		return $ret;
+	 }
+
+	 
+	//status  0:正常 1:上传  2：删除
+     static public function setPatientStatusByIds($ids,$hospital_id,$status,$manager_id){
+     	$ret=array(
+		   "ret"=>0,
+		   "msg"=>""
+		);
+		if($status!=1 && $status!=2){
+			$ret["ret"]=100;
+			$ret["msg"]="args err!";
+			return $ret;
+		}
+		for($i=0;$i<count($ids);$i++){
+			$ids[$i]=intval($ids[$i]);
+		}
+		$instr=implode(",",$ids);
+		$sql="update patientInfo  set status=:status ,lastmod_manager_id=:lastmod_manager_id where  hospital_id=:hospital_id and id in ($instr) and status=0  ";
+		
+		CUtil::logFile("=====$sql  ");
+		$args=array(':status'=>$status,':hospital_id'=>$hospital_id,":lastmod_manager_id"=>$manager_id);
+     	$connection = Yii::$app->db;
+		$command = $connection->createCommand($sql,$args);
+		$records = $command->execute();
+		if($records!=1){
+			$ret["ret"]=1;
+			$ret["msg"]="update err!";		
+			CUtil::logFile("=====$records  ");			
+			return $ret;
+		}
+		return $ret;
+
+     }
+
+      static public function getPatientList($page,$hospital_id,$filter,$size=10,$status=0)
+	 {
+		 $ret=array(
+		   "ret"=>0,
+		   
+		   "msg"=>""
+		);
+
+      
 
 
-
-
-
+/*
+		
+		$sql = "select * from patientInfo where id=:id";
+		$args=array(':id'=>$id);
+		CUtil::logFile("=====$id  ");
+		
+		$connection = Yii::$app->db;
+		$command = $connection->createCommand($sql,$args);
+		$records = $command->queryAll();
+		if(count($records)!=1){
+			$ret["ret"]=1;
+						
+			return $ret;
+		}
+		$ret["msg"]=$records[0];
+		return $ret;*/
+		return $ret;
+	 }
 
 }
