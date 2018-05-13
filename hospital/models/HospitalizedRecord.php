aa40b7d08437a0f2b7420043923295a08821acb7
bh841128 <390214830@qq.com>
2018-05-13 11:39:25 +0800

    fdgdfg

---
+++ hospital/models/HospitalizedRecord.php
@@ -0,0 +1,150 @@
+<?php
+
+namespace app\models;
+
+use Yii;
+use app\models\CError;
+use app\models\CUtil;
+
+class HospitalizedRecord {
+
+	static public function getRecordById($id,$hospital_id)
+	{
+		 $ret=array(
+		   "ret"=>0,
+		   "msg"=>""
+		);
+		
+		$sql = "select * from hospitalized_record where id=:id and hospital_id=:hospital_id ";
+		$args=array(':id'=>$id,':hospital_id'=>$hospital_id);
+		CUtil::logFile("=====$id  ");
+		
+		try{
+		$connection = Yii::$app->db;
+		$command = $connection->createCommand($sql,$args);
+		$records = $command->queryAll();
+		}catch(\Exception $ex){
+			$ret["ret"]=2;
+			$ret["msg"]=$ex->getCode()."  ".$ex->getMessage();;		
+			CUtil::logFile("===== ".$ret["msg"]);			
+			return $ret;
+		}
+		
+		
+		if(count($records)!=1){
+			$ret["ret"]=1;
+						
+			return $ret;
+		}
+		$ret["msg"]=$records[0];
+		return $ret;
+	}
+     
+	//status  1:正常 2:上传  3：删除
+     static public function setRecordStatusByIds($ids,$hospital_id,$status,$manager_id){
+     	$ret=array(
+		   "ret"=>0,
+		   "msg"=>""
+		);
+		if($status!=2 && $status!=3){
+			$ret["ret"]=100;
+			$ret["msg"]="args err!";
+			return $ret;
+		}
+		for($i=0;$i<count($ids);$i++){
+			$ids[$i]=intval($ids[$i]);
+		}
+		$instr=implode(",",$ids);
+		$now=time(0);
+		$sql="";
+		$args="";
+		if($status==2){
+			$sql="update hospitalized_record  set status=:status ,lastmodify_manager_id=:lastmodify_manager_id,lastmodifytime=:lastmodifytime where  hospital_id=:hospital_id and id in ($instr) and status=1  ";
+			$args=array(':status'=>$status,':hospital_id'=>$hospital_id,":lastmodify_manager_id"=>$manager_id,":lastmodifytime"=>$now);
+		}
+		else {//3
+		    $sql="update hospitalized_record  set status=:status ,lastmodify_manager_id=:lastmodify_manager_id,lastmodifytime=:lastmodifytime, uploadtime=:uploadtime where  hospital_id=:hospital_id and id in ($instr) and status=1  ";
+			$args=array(':status'=>$status,':hospital_id'=>$hospital_id,":lastmodify_manager_id"=>$manager_id,":lastmodifytime"=>$now,":uploadtime"=>$now);
+	
+		}
+		CUtil::logFile("=====$sql  ".print_r($args,true));
+		
+		try{
+     	$connection = Yii::$app->db;
+		$command = $connection->createCommand($sql,$args);
+		$records = $command->execute();
+		}catch(\Exception $ex){
+			$ret["ret"]=2;
+			$ret["msg"]=$ex->getCode()."  ".$ex->getMessage();;		
+			CUtil::logFile("===== ".$ret["msg"]);			
+			return $ret;
+		}
+		
+		
+		
+		
+		if($records!=1){
+			$ret["ret"]=1;
+			$ret["msg"]="update err!";		
+			CUtil::logFile("=====$records  ");			
+			return $ret;
+		}
+		return $ret;
+
+    }
+	
+	
+	 static public function getRecordList($page,$hospital_id,$filter,$size=10)
+	 {
+		 $ret=array(
+		   "ret"=>0,
+		   
+		   "msg"=>""
+		);
+         CUtil::logFile("=====$page   $hospital_id   $size");
+		
+        $sql = "select * from hospitalized_record where hospital_id=:hospital_id and ";
+		$args=array(":hospital_id"=>$hospital_id);
+		foreach ($filter as $key => $value) {  
+			if($key!="start_time"&&$key!="end_time"){
+				$sql=$sql." $key=:".$key."  and ";
+				$args[":".$key]=$value;
+			}
+			
+		}
+		/*
+		if(array_key_exists("end_time",$filter)){
+			$sql=$sql." uploadtime<=:end_time  and ";
+			$args[":end_time"]=$filter["end_time"];
+		}
+		if(array_key_exists("start_time",$filter)){
+			$sql=$sql." uploadtime>=:start_time   ";
+			$args[":start_time"]=$filter["start_time"];
+		}else{
+			$sql=$sql." uploadtime>=0 ";
+		}
+		*/
+		$page=$page<1?1:$page;
+		$sql=$sql." limit ".($page-1)*$size.",".$size;
+        CUtil::logFile("=====$sql  ".print_r($args,true));
+		
+		try{
+		$connection = Yii::$app->db;
+		$command = $connection->createCommand($sql,$args);
+		$records = $command->queryAll();
+		}catch(\Exception $ex){
+			$ret["ret"]=2;
+			$ret["msg"]=$ex->getCode()."  ".$ex->getMessage();;		
+			CUtil::logFile("===== ".$ret["msg"]);			
+			return $ret;
+		}
+		
+		$ret["msg"]=$records;
+		return $ret;
+
+	 }
+	 
+
+
+
+}
