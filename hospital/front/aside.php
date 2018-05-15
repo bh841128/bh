ac21376fa51970df582fa998cd14692567674b86
chaos <99918210@qq.com>
2018-05-15 22:45:02 +0800

    前端

---
+++ hospital/front/aside.php
@@ -0,0 +1,42 @@
+<aside class="main-sidebar" style="padding-top:0px">
+    <section class="sidebar">
+        <ul class="sidebar-menu" data-widget="tree" id="sidebar-menu">
+            <li class="treeview" style="margin-bottom:10px">
+                <span class="gray-font" style="font-size:17px;font-weight:bold;">信息管理</span>
+            </li>
+            <li class="treeview">
+                <div class="tree-btn-wrapper">
+                    <div class="btn gray-font tree-btn active">
+                        <span class="glyphicon glyphicon-plus"></span>&nbsp;&nbsp;新增资料</div>
+                </div>
+            </li>
+            <li class="treeview">
+                <div class="tree-btn-wrapper">
+                    <div class="btn gray-font tree-btn">
+                        <span class="glyphicon glyphicon-upload"></span>&nbsp;&nbsp;上传资料</div>
+                </div>
+            </li>
+            <li class="treeview">
+                <div class="tree-btn-wrapper">
+                    <div class="btn gray-font tree-btn">
+                        <span class="glyphicon glyphicon-search"></span>&nbsp;&nbsp;数据查询</div>
+                </div>
+            </li>
+            <li class="treeview">
+                <div class="tree-btn-wrapper">
+                    <div class="btn gray-font tree-btn">
+                        <span class="glyphicon glyphicon-export"></span>&nbsp;&nbsp;数据导出</div>
+                </div>
+            </li>
+            <li class="treeview" style="margin-bottom:10px;margin-top:10px">
+                <span class="gray-font" style="font-size:17px;font-weight:bold;">数据统计</span>
+            </li>
+            <li class="treeview">
+                <div class="tree-btn-wrapper">
+                    <div class="btn gray-font tree-btn">
+                        <span class="glyphicon glyphicon-list-alt"></span>&nbsp;&nbsp;数据报表</div>
+                </div>
+            </li>
+        </ul>
+    </section>
+</aside>
---
+++ hospital/front/header.php
@@ -0,0 +1,28 @@
+<header class="main-header">
+    <nav class="navbar navbar-static-top" role="navigation" style="margin-left:0px;">
+        <a href="#" class="logo" style="width:500px">
+            <img src="./img/top-banner.png" />
+        </a>
+        <div class="navbar-custom-menu">
+            <ul class="nav navbar-nav">
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle gray-font" style="font-size:18px" data-toggle="dropdown">中国医学科学院阜外医院
+                        <span class="caret"></span>
+                    </a>
+                    <ul class="dropdown-menu" role="menu" style="min-width:10px" id="user_login_menu">
+                        <li>
+                            <a href="#" data-toggle="control-sidebar" title="Sign out" tag="change_password">
+                                <i class="fa fa-sign-out">修改密码</i>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#" data-toggle="control-sidebar" title="Sign out" tag="signout">
+                                <i class="fa fa-power-off">退出</i>
+                            </a>
+                        </li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</header>
