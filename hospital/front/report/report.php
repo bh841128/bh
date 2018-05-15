8d62709776660b070947b3383e88c2988b07efde
chaos <99918210@qq.com>
2018-05-16 01:51:16 +0800

    前端

---
+++ hospital/front/report/report.php
@@ -0,0 +1,37 @@
+<div class="report-chart" style="position:relative;width:100%;height:700px;margin-top:20px">
+    <div class="report-chart-title" style="text-align:center">
+        <h3>少儿先天性心脏病月录入量统计</h3>
+        <h4 style="padding-top:10px">总量：1234</h4>
+    </div>
+    <div style="position: absolute;top:20px;right:10px;">
+        <select class="form-control input-sm" style="width:100px">
+            <option value="2018">2018</option>
+        </select>
+    </div>
+    <div class="report-chart-container" style="height:500px;" id="report-chart">
+    </div>
+    <div class="report-chart-footer" style="text-align:left;">
+        <div class="form-horizontal">
+            <table class="control-table padding-20">
+                <tr>
+                    <td>
+                        <div style="padding-left:200px">
+                            <select class="form-control input-sm" style="width:150px">
+                                <option value="医院">医院</option>
+                            </select>
+                        </div>
+
+                    </td>
+                    <td>
+                        <div style="height:13px;width:13px;margin-left:20px;margin-right:10px;background-color:rgb(86,187,251)"></div>
+                    </td>
+                    <td>阜外医院</td>
+                    <td>
+                        <div style="height:13px;width:13px;margin-left:20px;margin-right:10px;background-color:rgb(255,129,73)"></div>
+                    </td>
+                    <td>其他</td>
+                </tr>
+            </table>
+        </div>
+    </div>
+</div>
--- hospital/web/report.php
+++ hospital/web/report.php
@@ -20,83 +20,42 @@ require_once(__DIR__."/../config/front_config.php");
             <!-- Main content -->
             <section class="content container-fluid" style="min-height:650px">
                 <div class="tab-content">
-                    <div class="report-chart" style="position:relative;width:100%;height:700px;margin-top:20px">
-                        <div class="report-chart-title" style="text-align:center">
-                            <h3>少儿先天性心脏病月录入量统计</h3>
-                            <h4 style="padding-top:10px">总量：1234</h4>
-                        </div>
-                        <div style="position: absolute;top:20px;right:10px;">
-                            <select class="form-control input-sm" style="width:100px">
-                                <option value="2018">2018</option>
-                            </select>
-                        </div>
-                        <div class="report-chart-container" style="height:500px;" id="report-chart">
-
-                        </div>
-                        <div class="report-chart-footer" style="text-align:left;">
-                            <div class="form-horizontal">
-                                <table class="control-table padding-20">
-                                    <tr>
-                                        <td>
-                                            <div style="padding-left:200px">
-                                                <select class="form-control input-sm" style="width:150px">
-                                                    <option value="医院">医院</option>
-                                                </select>
-                                            </div>
-
-                                        </td>
-                                        <td>
-                                            <div style="height:13px;width:13px;margin-left:20px;margin-right:10px;background-color:rgb(86,187,251)"></div>
-                                        </td>
-                                        <td>阜外医院</td>
-                                        <td>
-                                            <div style="height:13px;width:13px;margin-left:20px;margin-right:10px;background-color:rgb(255,129,73)"></div>
-                                        </td>
-                                        <td>其他</td>
-                                    </tr>
-                                </table>
-                            </div>
-
-                        </div>
-                    </div>
+                    <?php require(WEB_PAGE_PATH."export/export.php"); ?>
+                </div>
             </section>
-            </div>
-            <!-- /.content-wrapper -->
         </div>
         <?php require(WEB_PAGE_PATH."footer.php"); ?>
         <?php require(WEB_PAGE_PATH."login_modal.php"); ?>
-        <?php require(WEB_PAGE_PATH."js.php"); ?>
-        <script src="/web/lib/adminlte/js/raphael.min.js"></script>
-        <script src="/web/lib/adminlte/js/morris.min.js"></script>
-        
-        <script type="text/javascript">
-            initPage();
-            var bar = new Morris.Bar({
-                element: 'report-chart',
-                resize: true,
-                data: [
-                    { y: '01月份', "阜外医院": 1000, "其他": 900 },
-                    { y: '02月份', "阜外医院": 100, "其他": 90 },
-                    { y: '03月份', "阜外医院": 200, "其他": 40 },
-                    { y: '04月份', "阜外医院": 200, "其他": 40 },
-                    { y: '05月份', "阜外医院": 2000, "其他": 400 },
-                    { y: '06月份', "阜外医院": 200, "其他": 40 },
-                    { y: '07月份', "阜外医院": 200, "其他": 40 },
-                    { y: '08月份', "阜外医院": 200, "其他": 40 },
-                    { y: '09月份', "阜外医院": 600, "其他": 220 },
-                    { y: '10月份', "阜外医院": 200, "其他": 40 },
-                    { y: '11月份', "阜外医院": 200, "其他": 40 },
-                    { y: '12月份', "阜外医院": 700, "其他": 240 }
-                ],
-                barColors: ['rgb(86,187,251)', '#f56954'],
-                xkey: 'y',
-                ykeys: ['阜外医院', '其他'],
-                labels: ['阜外医院', '其他'],
-                hideHover: 'auto'
-            });
-        </script>
     </div>
-
+    <?php require(WEB_PAGE_PATH."js.php"); ?>
+    <script src="/web/lib/adminlte/js/raphael.min.js"></script>
+    <script src="/web/lib/adminlte/js/morris.min.js"></script>
+    <script type="text/javascript">
+        initPage();
+        var bar = new Morris.Bar({
+            element: 'report-chart',
+            resize: true,
+            data: [
+                { y: '01月份', "阜外医院": 1000, "其他": 900 },
+                { y: '02月份', "阜外医院": 100, "其他": 90 },
+                { y: '03月份', "阜外医院": 200, "其他": 40 },
+                { y: '04月份', "阜外医院": 200, "其他": 40 },
+                { y: '05月份', "阜外医院": 2000, "其他": 400 },
+                { y: '06月份', "阜外医院": 200, "其他": 40 },
+                { y: '07月份', "阜外医院": 200, "其他": 40 },
+                { y: '08月份', "阜外医院": 200, "其他": 40 },
+                { y: '09月份', "阜外医院": 600, "其他": 220 },
+                { y: '10月份', "阜外医院": 200, "其他": 40 },
+                { y: '11月份', "阜外医院": 200, "其他": 40 },
+                { y: '12月份', "阜外医院": 700, "其他": 240 }
+            ],
+            barColors: ['rgb(86,187,251)', '#f56954'],
+            xkey: 'y',
+            ykeys: ['阜外医院', '其他'],
+            labels: ['阜外医院', '其他'],
+            hideHover: 'auto'
+        });
+    </script>
 </body>
 
 </html>
