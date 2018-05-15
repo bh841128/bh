11edaf5a68bdc372c97522e69b14d45335b4a05b
chaos <99918210@qq.com>
2018-05-16 02:38:31 +0800

    前端

--- hospital/web/web/js/control_json.js
+++ hospital/web/web/js/control_json.js
@@ -2,9 +2,9 @@ function control_json(){
     /////扫码节点下所有的json
     this.parseControlJson = function(selector){
         var input_values = [];
-        select.find("input[json-name]").each(function(){
+        selector.find("input[json-name]").each(function(){
             var json_name = $(this).attr("json-name");
-            var json_value = "";
+            var json_value = getInputValue($(this));
             var input_value = {"json-name":json_name,"json_value":json_value};
             input_values.push(input_value);
         })
@@ -15,5 +15,10 @@ function control_json(){
     /////将json值赋给节点
     this.setJson2Control = function(selector, json){
 
+    }
+
+    /////////////////////////////////////////////////////////////////
+    function getInputValue(control){
+        return "";
     }
 }
