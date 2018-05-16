03bbba7e5f46f296f927026502170d9ea167f221
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-16 15:23:49 +0800

    前端

--- hospital/front/js.php
+++ hospital/front/js.php
@@ -3,7 +3,9 @@
 <script src="/web/lib/adminlte/js/adminlte.min.js"></script>
 <script src="/web/lib/moment-with-locales.min.js"></script>
 <script src="/web/lib/datetimepicker/bootstrap-datetimepicker.min.js"></script>
+<script src="/web/lib/jquery.placeholder"></script>
 <script src="/web/lib/jquery.json.js"></script>
+
 <script src="/web/js/public.js"></script>
 <script src="/web/js/address.js"></script>
 <script src="/web/js/control_json.js"></script>
