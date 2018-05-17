8f3a3b4754732a77945df2eb94ebbaf40969320e
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-17 11:13:37 +0800

    前端

--- hospital/web/web/js/query.js
+++ hospital/web/web/js/query.js
@@ -3,6 +3,9 @@ function patient_query(){
     this.m_options = {};
     this.m_data = {};
     this.m_query_param = {};
+
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////业务相关
     //"序号","病案号","姓名","性别", "出生日期", "联系人", "联系电话", "医院", "上传时间", "状态", "--操作,删除"
     this.map_showname_name = {
         "序号":"id",
@@ -16,7 +19,71 @@ function patient_query(){
         "上传时间":"uploadtime",
         "状态":"status"
     }
-    
+    this.m_query_data_api = "hospital/get-patient-list";
+    function preProcessData(rawDatas){
+        for (var i = 0; i < rawDatas.length; i++){
+        try{
+            data[i]["relate_text"] = eval('('+rawDatas[i]["relate_text"]+')');
+        }
+        catch (err) {
+            data[i]["relate_text"] = {};
+        }
+    }
+    function transData(table_datas){
+        for (var i = 0; i < table_datas.length; i++){
+            table_datas[i]["医院"] = getHospitalName(table_datas[i]["医院"]);
+            table_datas[i]["状态"] = getStatusName(table_datas[i]["状态"]);
+            table_datas[i]["上传时间"] = timestampToString(table_datas[i]["上传时间"]);
+            table_datas[i]["性别"] = getXingbieName(table_datas[i]["性别"]);
+        }
+    }
+    function getHospitalName(hospital_id){
+        var hospitals = [
+            {"name":"中国医学科学院阜外医院","id":"1"},
+            {"name":"上海交通大学医学院附属上海儿童医学中心","id":"2"},
+            {"name":"复旦大学附属儿科医院","id":"3"},
+            {"name":"中南大学湘雅二医院","id":"4"},
+            {"name":"南京市儿童医院","id":"5"},
+            {"name":"青岛市妇女儿童医院","id":"6"},
+            {"name":"河南省人民医院","id":"7"},
+            {"name":"广州市妇女儿童医疗中心","id":"8"},
+            {"name":"中国人民解放军第四军医大学第一附属医院","id":"9"},
+            {"name":"中国人民解放军第三军医大学","id":"10"}
+        ];
+        for (var i = 0; i < hospitals.length; i++){
+            if (hospitals[i]["id"] == hospital_id){
+                return hospitals[i]["name"];
+            }
+        }
+        return ""+hospital_id;
+    }
+    function getStatusName(status){
+        var status_map = [
+            {"name":"正常","id":"1"},
+            {"name":"上传","id":"2"},
+            {"name":"删除","id":"3"}
+        ];
+        for (var i = 0; i < status_map.length; i++){
+            if (status_map[i]["id"] == status){
+                return status_map[i]["name"];
+            }
+        }
+        return ""+status;
+    }
+    function getXingbieName(xingbie){
+        var xingbie_map = [
+            {"name":"男","id":"1"},
+            {"name":"女","id":"2"}
+        ];
+        for (var i = 0; i < xingbie_map.length; i++){
+            if (xingbie_map[i]["id"] == xingbie){
+                return xingbie_map[i]["name"];
+            }
+        }
+        return ""+xingbie;
+    }
+    ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    //接口
     this.init = function(options){
         m_this.m_options = options;
         if (typeof m_this.m_options["nav_page_num"] == "undefined"){
@@ -24,8 +91,8 @@ function patient_query(){
         }
         initPageNav(m_this.m_options.page_nav_wrapper, m_this.m_options);
     }
-    this.queryPatient = function(query_param){
-        function queryPatientRet(rsp){
+    this.queryData = function(query_param){
+        function queryDataRet(rsp){
             console.dir(rsp);
             if (rsp.ret != 0){
                 alert("拉取数据错误，请稍候重试");
@@ -48,7 +115,7 @@ function patient_query(){
         }
         m_this.m_query_param = query_param;
         
-        ajaxRemoteRequest("hospital/get-patient-list",m_this.m_query_param,queryPatientRet);
+        ajaxRemoteRequest(m_this.m_query_data_api,m_this.m_query_param,queryDataRet);
     }
 
     ////////////////////////////////////////////////////////////////////////////////////////////
@@ -78,13 +145,9 @@ function patient_query(){
     }
 
     function fillTable(data, options){
+        preProcessData(data.records);
         var table_datas = getTableShowData(data.records, options.show_fields);
-        for (var i = 0; i < table_datas.length; i++){
-            table_datas[i]["医院"] = getHospitalName(table_datas[i]["医院"]);
-            table_datas[i]["状态"] = getStatusName(table_datas[i]["状态"]);
-            table_datas[i]["上传时间"] = timestampToString(table_datas[i]["上传时间"]);
-            table_datas[i]["性别"] = getXingbieName(table_datas[i]["性别"]);
-        }
+        transData(table_datas);
         console.dir(table_datas);
         var table_html = getTableHtml(table_datas, options);
         options.table_wrapper.html(table_html);
@@ -92,8 +155,6 @@ function patient_query(){
             updatePageNav(data.total_num, data.page_size, data.cur_page, options.page_nav_wrapper);
         }
     }
-
-    
     ////////////////////////////////////////////////////////////////////////////////////////////////
     function getJsonValueByShowname(json, show_name){
         if (typeof m_this.map_showname_name[show_name] == "undefined"){
@@ -116,13 +177,6 @@ function patient_query(){
     function getTableShowData(data, show_fields){
         var table_datas = [];
         for (var i = 0; i < data.length; i++){
-            try{
-                data[i]["relate_text"] = eval('('+data[i]["relate_text"]+')');
-            }
-            catch (err) {
-                data[i]["relate_text"] = {};
-            }
-            
             var table_data = {};
             for (var f = 0; f < show_fields.length; f++){
                 var show_name = show_fields[f];
@@ -172,51 +226,6 @@ function patient_query(){
         table_html += '</tbody>';
         table_html += '</table>'
         return table_html;
-    }
-    function getHospitalName(hospital_id){
-        var hospitals = [
-            {"name":"中国医学科学院阜外医院","id":"1"},
-            {"name":"上海交通大学医学院附属上海儿童医学中心","id":"2"},
-            {"name":"复旦大学附属儿科医院","id":"3"},
-            {"name":"中南大学湘雅二医院","id":"4"},
-            {"name":"南京市儿童医院","id":"5"},
-            {"name":"青岛市妇女儿童医院","id":"6"},
-            {"name":"河南省人民医院","id":"7"},
-            {"name":"广州市妇女儿童医疗中心","id":"8"},
-            {"name":"中国人民解放军第四军医大学第一附属医院","id":"9"},
-            {"name":"中国人民解放军第三军医大学","id":"10"}
-        ];
-        for (var i = 0; i < hospitals.length; i++){
-            if (hospitals[i]["id"] == hospital_id){
-                return hospitals[i]["name"];
-            }
-        }
-        return ""+hospital_id;
-    }
-    function getStatusName(status){
-        var status_map = [
-            {"name":"正常","id":"1"},
-            {"name":"上传","id":"2"},
-            {"name":"删除","id":"3"}
-        ];
-        for (var i = 0; i < status_map.length; i++){
-            if (status_map[i]["id"] == status){
-                return status_map[i]["name"];
-            }
-        }
-        return ""+status;
-    }
-    function getXingbieName(xingbie){
-        var xingbie_map = [
-            {"name":"男","id":"1"},
-            {"name":"女","id":"2"}
-        ];
-        for (var i = 0; i < xingbie_map.length; i++){
-            if (xingbie_map[i]["id"] == xingbie){
-                return xingbie_map[i]["name"];
-            }
-        }
-        return ""+xingbie;
     }
 }
 
