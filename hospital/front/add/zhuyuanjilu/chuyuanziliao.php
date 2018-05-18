e01c8a3cb52ae36ecdd2f7b84ac8fda30928886f
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-18 15:33:14 +0800

    前端

--- hospital/front/add/zhuyuanjilu/chuyuanziliao.php
+++ hospital/front/add/zhuyuanjilu/chuyuanziliao.php
@@ -19,11 +19,11 @@
                 </td>
                 <td>
                     <lable class="radio-inline">
-                        <input type="radio" name="chuyuanzhuangtai" value="1" checked>存活</lable>
+                        <input type="radio" name="chuyuanzhuangtai" value="1" checked json-name="出院状态">存活</lable>
                     <lable class="radio-inline">
-                        <input type="radio" name="chuyuanzhuangtai" value="2">死亡</lable>
+                        <input type="radio" name="chuyuanzhuangtai" value="2" json-name="出院状态">死亡</lable>
                     <lable class="radio-inline">
-                        <input type="radio" name="chuyuanzhuangtai" value="3">自动出院</lable>
+                        <input type="radio" name="chuyuanzhuangtai" value="3" json-name="出院状态">自动出院</lable>
                 </td>
             </tr>
         </table>
@@ -34,7 +34,7 @@
                 </td>
                 <td>
                     <div class="input-group date">
-                        <input type="text" class="form-control input-sm" tag="datepicker" style="width:160px">
+                        <input type="text" class="form-control input-sm" tag="datepicker" style="width:160px" json-name="死亡日期">
                         <span class="input-group-addon">
                             <span class="glyphicon glyphicon-calendar"></span>
                         </span>
@@ -49,7 +49,7 @@
                 </td>
                 <td>
                     <div class="input-group">
-                        <select class="form-control input-sm" style="width:300px"></select>
+                        <select class="form-control input-sm" style="width:300px" json-name="死亡原因"></select>
                     </div>
                 </td>
             </tr>
@@ -60,7 +60,7 @@
                     <div class="control-label" style="width:124px">术后住院时间：</div>
                 </td>
                 <td>
-                    <input type="text" class="form-control input-sm" style="width:100px" placeholder="">
+                    <input type="text" class="form-control input-sm" style="width:100px" placeholder="术后住院时间" json-name="术后住院时间">
                 </td>
                 <td>
                     <span>（天）</span>
@@ -74,7 +74,7 @@
                 </td>
                 <td>
                     <div class="input-group date">
-                        <input type="text" class="form-control input-sm" tag="datepicker" style="width:160px">
+                        <input type="text" class="form-control input-sm" tag="datepicker" style="width:160px" json-name="自动出院日期">
                         <span class="input-group-addon">
                             <span class="glyphicon glyphicon-calendar"></span>
                         </span>
@@ -89,7 +89,7 @@
                 </td>
                 <td>
                     <div class="input-group">
-                        <select class="form-control input-sm" style="width:300px"></select>
+                        <select class="form-control input-sm" style="width:300px" json-name="自动出院原因"></select>
                     </div>
                 </td>
             </tr>
@@ -100,7 +100,7 @@
                     <div class="control-label" style="width:124px">治疗费用：</div>
                 </td>
                 <td>
-                    <input type="text" class="form-control input-sm" style="width:100px" placeholder="">
+                    <input type="text" class="form-control input-sm" style="width:100px" placeholder="治疗费用" json-name="治疗费用">
                 </td>
                 <td>
                     <span>（元）</span>
@@ -113,7 +113,7 @@
                     <div class="control-label" style="width:124px">备注：</div>
                 </td>
                 <td>
-                    <input type="text" class="form-control input-sm" style="width:400px" placeholder="">
+                    <input type="text" class="form-control input-sm" style="width:400px" placeholder="备注" json-name="备注">
                 </td>
             </tr>
         </table>
