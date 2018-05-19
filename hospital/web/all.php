baf42e224afaba1608f7d20eeb9d622a36e1e69c
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-19 12:18:42 +0800

    前端

--- hospital/web/all.php
+++ hospital/web/all.php
@@ -11,6 +11,13 @@ require_once(__DIR__."/../config/front_config.php");
     <div class="top-hr"></div>
     <div class="wrapper top-wrapper">
         <?php require(WEB_PAGE_PATH."aside.php"); ?>
+        <div class="hospital-breadcrumb">
+            <ol class="breadcrumb">
+                <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
+                <li><a href="#">Forms</a></li>
+                <li class="active">General Elements</li>
+            </ol>
+        </div>
     </div>
 </body>
 
