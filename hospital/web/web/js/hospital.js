f97c114a55b63e69de7fe2c108dbbe9b29314ddc
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-19 13:37:02 +0800

    前端

--- hospital/web/web/js/hospital.js
+++ hospital/web/web/js/hospital.js
@@ -1,7 +1,16 @@
 function hospital(){
+	this.breadCrumbData = {
+		pages : [
+			{"name":"新增资料","icon":"glyphicon-plus", pages:[
+				
+			]}
+		],
+		currentSite:[]
+	}
 	this.init = function(){
 	}
-	this.gotoPage = function(){
+	this.gotoPage = function(dstSite){
+
 	}
 }
 
