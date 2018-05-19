5b438255d68d11a35c6567ca1bdbcb72c70b3a90
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-19 18:21:21 +0800

    前端

---
+++ hospital/front/breadcrumb.php
@@ -0,0 +1,11 @@
+<div class="bread-content-wrapper">
+    <section class="content-header">
+        <div class="hospital-breadcrumb">
+            <ol class="breadcrumb" id="breadcrumb">
+                <li bread-level="1" style="display:none"></li>
+                <li bread-level="2" style="display:none"></li>
+                <li bread-level="3" style="display:none"></li>
+            </ol>
+        </div>
+    </section>
+</div>
--- hospital/web/all.php
+++ hospital/web/all.php
@@ -13,17 +13,7 @@ require_once(__DIR__."/../config/front_config.php");
         <div class="top-wrapper2">
             <?php require(WEB_PAGE_PATH."aside.php"); ?>
             <div class="hospital-wrapper">
-                <div class="bread-content-wrapper">
-                    <section class="content-header">
-                        <div class="hospital-breadcrumb">
-                            <ol class="breadcrumb" id="breadcrumb">
-                                <li bread-level="1" style="display:none"></li>
-                                <li bread-level="2" style="display:none"></li>
-                                <li bread-level="3" style="display:none"></li>
-                            </ol>
-                        </div>
-                    </section>
-                </div>
+                <?php require(WEB_PAGE_PATH."breadcrumb.php"); ?>
                 <div class="hospital-content-wrapper">
                 </div>
             </div>
