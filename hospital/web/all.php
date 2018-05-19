8b4b49e4fc09a74e005a22035a495c2544cfbf02
chaos <99918210@qq.com>
2018-05-20 01:36:18 +0800

    前端

--- hospital/web/all.php
+++ hospital/web/all.php
@@ -4,6 +4,7 @@ defined('YII_ENV') or define('YII_ENV', 'dev');
 
 require __DIR__ . '/../vendor/autoload.php';
 require __DIR__ . '/../vendor/yiisoft/yii2/Yii.php';
+$config = require __DIR__ . '/../config/web.php';
 $application = new yii\web\Application($config);
 exit;
 require __DIR__ . '/../models/Hospital.php';
