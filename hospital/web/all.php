acc20fa63b9bca064706f10f253b2772ec3a8c25
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-19 15:04:28 +0800

    前端

--- hospital/web/all.php
+++ hospital/web/all.php
@@ -8,30 +8,10 @@ require_once(__DIR__."/../config/front_config.php");
 </head>
 <body class="hold-transition skin-blue sidebar-mini">
     <?php require(WEB_PAGE_PATH."header.php"); ?>
-    <div class="top-hr"></div>
     <div class="top-wrapper">
-        
-        <div class="content-wrapper bread-content-wrapper">
-            <section class="content-header">
-                <div class="hospital-breadcrumb">
-                    <ol class="breadcrumb">
-                        <li><a href="#"><i class="fa fa-dashboard"></i> Home</a></li>
-                        <li><a href="#">Forms</a></li>
-                        <li class="active">General Elements</li>
-                    </ol>
-                </div>
-            </section>
-        </div>
 
         <?php require(WEB_PAGE_PATH."footer.php"); ?>
     </div>
-
-     
-    <?php require(WEB_PAGE_PATH."js.php"); ?>
-    <script type="text/javascript">
-        var g_hospital = new hospital();
-        g_hospital.init();
-    </script>
 </body>
 
 </html>
