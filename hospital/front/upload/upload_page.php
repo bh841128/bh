6774325f85a648d8350af749dd908153d76b26e8
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-19 18:59:59 +0800

    前端

---
+++ hospital/front/upload/upload_page.php
@@ -0,0 +1,7 @@
+<section class="content container-fluid main-container">
+    <div class="tab-content">
+        <div class="tab-pane form-step fade active in">
+            <?php require(WEB_PAGE_PATH."upload/upload.php"); ?>
+        </div>
+    </div>
+</section>
