ea182e3003e7e04c42cb2c49dbcc248aa7f5ba29
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-19 16:17:10 +0800

    前端

--- hospital/front/footer.php
+++ hospital/front/footer.php
@@ -1,15 +1,17 @@
-<footer>
-    <div class="copyright">
-        <p>保险业务经营许可证：269633000000800 小雨伞保险经纪有限公司：9112011608300716X9</p>
-        <p>版权所有 ©
-            <span id="footerYear">2018</span> 小雨伞保险经纪有限公司
-            <a href="http://www.miibeian.gov.cn" target="_blank" rel="nofollow">津ICP备17005385号-3</a>
-        </p>
-        <div>
-            <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=12019202000239" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
-                <img src="/web/img/beian.png" style="float:left;">
-                <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">津公网安备 12019202000239号</p>
-            </a>
+<div class="hospital-footer">
+    <footer>
+        <div class="copyright">
+            <p>保险业务经营许可证：269633000000800 小雨伞保险经纪有限公司：9112011608300716X9</p>
+            <p>版权所有 ©
+                <span id="footerYear">2018</span> 小雨伞保险经纪有限公司
+                <a href="http://www.miibeian.gov.cn" target="_blank" rel="nofollow">津ICP备17005385号-3</a>
+            </p>
+            <div>
+                <a target="_blank" href="http://www.beian.gov.cn/portal/registerSystemInfo?recordcode=12019202000239" style="display:inline-block;text-decoration:none;height:20px;line-height:20px;">
+                    <img src="/web/img/beian.png" style="float:left;">
+                    <p style="float:left;height:20px;line-height:20px;margin: 0px 0px 0px 5px; color:#939393;">津公网安备 12019202000239号</p>
+                </a>
+            </div>
         </div>
-    </div>
-</footer>
+    </footer>
+</div>
