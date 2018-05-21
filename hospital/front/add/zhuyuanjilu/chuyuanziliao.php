c836b8b147e5eee96c8d4b530c45d12b6f166ca6
chaos <99918210@qq.com>
2018-05-22 04:41:52 +0800

    前端

--- hospital/front/add/zhuyuanjilu/chuyuanziliao.php
+++ hospital/front/add/zhuyuanjilu/chuyuanziliao.php
@@ -15,15 +15,15 @@
         <table class="control-table padding-20">
             <tr>
                 <td>
-                    <div class="control-label" style="width:124px">出院状态：</div>
+                    <div class="control-label" style="width:124px">出院时状态：</div>
                 </td>
                 <td>
                     <lable class="radio-inline">
-                        <input type="radio" name="chuyuanzhuangtai" value="0" checked json-name="出院状态">存活</lable>
+                        <input type="radio" name="chuyuanzhuangtai" value="0" checked json-name="出院时状态">存活</lable>
                     <lable class="radio-inline">
-                        <input type="radio" name="chuyuanzhuangtai" value="1" json-name="出院状态">死亡</lable>
+                        <input type="radio" name="chuyuanzhuangtai" value="1" json-name="出院时状态">死亡</lable>
                     <lable class="radio-inline">
-                        <input type="radio" name="chuyuanzhuangtai" value="2" json-name="出院状态">自动出院</lable>
+                        <input type="radio" name="chuyuanzhuangtai" value="2" json-name="出院时状态">自动出院</lable>
                 </td>
             </tr>
         </table>
@@ -77,14 +77,14 @@
                 </td>
             </tr>
         </table>
-        <table class="control-table padding-20" tag="自动出院原因">
+        <table class="control-table padding-20" tag="自动出院主要原因">
             <tr>
                 <td>
                     <div class="control-label" style="width:124px">自动出院原因：</div>
                 </td>
                 <td>
                     <div class="input-group">
-                        <select class="form-control input-sm" style="width:300px" json-name="自动出院原因">
+                        <select class="form-control input-sm" style="width:300px" json-name="自动出院主要原因">
                             <option value="心脏原因">心脏原因</option>
                             <option value="神经系统原因">神经系统原因</option>
                             <option value="肾脏原因">肾脏原因</option>
@@ -111,16 +111,5 @@
                 </td>
             </tr>
         </table>
-        <table class="control-table padding-20">
-            <tr>
-                <td>
-                    <div class="control-label" style="width:124px">备注：</div>
-                </td>
-                <td>
-                    <input type="text" class="form-control input-sm" style="width:400px" placeholder="备注" json-name="备注">
-                </td>
-            </tr>
-        </table>
-
     </div>
 </div>
