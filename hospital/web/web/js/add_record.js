d74c0eb6d8ec6cebb616c871bcdf33928944108e
chaos <99918210@qq.com>
2018-05-23 08:14:47 +0800

    前端

--- hospital/web/web/js/add_record.js
+++ hospital/web/web/js/add_record.js
@@ -465,13 +465,13 @@ function addZhuyuanjilu(){
 		var operation_before_info = data_json.operation_before_info;
 		for (var i = 1; i <= operation_before_info["既往心脏病手术次数"]; i++){
 			if (operation_before_info["既往心脏病手术时间-不能提供-"+i] <= 0){
-				checkValueValid(arr_errmsgs, data_json, "既往心脏病手术时间-"+i,		"不能为空",		"请填写 既往心脏病手术时间"+i);
+				checkValueValid(arr_errmsgs, operation_before_info, "既往心脏病手术时间-"+i,		"不能为空",		"请填写 既往心脏病手术时间"+i);
 			}
 			if (operation_before_info["既往心脏病手术医院-不能提供-"+i] <= 0){
-				checkValueValid(arr_errmsgs, data_json, "既往心脏病手术医院-"+i,		"不能为空",		"请填写 既往心脏病手术医院"+i);
+				checkValueValid(arr_errmsgs, operation_before_info, "既往心脏病手术医院-"+i,		"不能为空",		"请填写 既往心脏病手术医院"+i);
 			}
 			if (operation_before_info["既往心脏病手术名称-不能提供-"+i] <= 0){
-				checkValueValid(arr_errmsgs, data_json, "既往心脏病手术名称-"+i,		"不能为空",		"请填写 既往心脏病手术名称"+i);
+				checkValueValid(arr_errmsgs, operation_before_info, "既往心脏病手术名称-"+i,		"不能为空",		"请填写 既往心脏病手术名称"+i);
 			}
 			
 		}
