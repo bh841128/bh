1fe508641b7b6caa514d65f03e7765724220763b
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-25 13:03:14 +0800

    添加

--- hospital/front/add/zhuyuanjilu/save_upload.php
+++ hospital/front/add/zhuyuanjilu/save_upload.php
@@ -1,6 +1,6 @@
 <div class="form-horizontal">
     <div class="form-group errormsg active"><div class="msg-wrap"><div class="msg-error"><b></b></div></div></div>
-    <div style="text-align:left;margin-left:100px;margin-top:30px;">
+    <div style="text-align:left;margin-left:100px;margin-top:5px;">
         <button type="button" class="btn btn-primary" style="width:100px" tag="zyjl-save">保存</button>&nbsp;&nbsp;
         <button type="button" class="btn btn-primary" style="width:100px" tag="zyjl-upload">上传</button>&nbsp;&nbsp;
         <button type="button" class="btn btn-primary" style="width:100px" tag="zyjl-return">返回</button>
