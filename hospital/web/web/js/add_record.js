ce4a012b9cbac89cca2a4f6558641d9e7a324769
chaosliu(刘肖震) <chaosliu@tencent.com>
2018-05-25 13:05:20 +0800

    添加

--- hospital/web/web/js/add_record.js
+++ hospital/web/web/js/add_record.js
@@ -756,7 +756,7 @@ function addZhuyuanjilu(){
 			return false;
 		}
 		var arr_need_check = save_need_check.split(",");
-		for (var i = 0; i < arr_need_check; i++){
+		for (var i = 0; i < arr_need_check.length; i++){
 			var check_step = arr_need_check[i];
 			var arr_errmsgs = [];
 			if (check_step == 1){
